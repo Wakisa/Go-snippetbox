f1061df4f849c8b9cc1dbc8dac49ad940ae296c4
Wakisa Mwabulambo <wakielambo@gmail.com>
2024-11-19 14:35:50 +0200

    After adding Cookies and redirections

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -20,13 +21,6 @@ func newTestApplication(*testing.T) *application {
 // Define a custom testServer type which embeds a httptest.Server instance.
 type testServer struct {
 	*httptest.Server
-}
-
-// Create a new TestServer helper which initalizes and returns a new instance
-// of our custom testServer type.
-func newTestServer(t *testing.T, h http.Handler) *testServer {
-	ts := httptest.NewTLSServer(h)
-	return &testServer{ts}
 }
 
 // Implement a get() method on our custom testServer type. This makes a GET
@@ -46,4 +40,30 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, strin
 	body = bytes.TrimSpace(body)
 
 	return rs.StatusCode, rs.Header, string(body)
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	// Initalize the test server as normal.
+	ts := httptest.NewTLSServer(h)
+
+	// Initalaize a new cookie jar.
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the cookie jar to the test server client. Any response cookies will
+	// now be stored and sent with subsequent requests when using this client.
+	ts.Client().Jar = jar
+
+	// Disable redirect-folowing for the test server client by setting a custom
+	// CheckRedirect function. This function will be called whenever a 3xx
+	// response is received by the client, and by always returning a
+	// http.ErrUseLastResponse error it forces the client to immediately return
+	// the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	return &testServer{ts}
 }
