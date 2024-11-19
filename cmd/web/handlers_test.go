e6d379133aeccfc941454e3d05e2ec6e8bc18582
Wakisa Mwabulambo <wakielambo@gmail.com>
2024-11-19 14:14:52 +0200

    After adding test Helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"wakisa.com/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	// Create a new instance of our application struct. For now, this just
-	// contains a structured logger (which discards anything written to it).
-	app := &application{
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
+	app := newTestApplication(t)
 
-	// We then use the httptest.NewTLSServer() function to create a new test
-	// server, passing in the value returned by our app.routes() method as the
-	// handler for the server. This starts up a HTTPS server which listens on a
-	// randomly-chosen port of your local machine for the duration of the test.
-	// Notice that we defer a call to ts.Close() so that the server is shutdown
-	// when the test finishes.
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	// The network address that the test server is listening on is contained in
-	// the ts.URL field. We can use this along with ts.Client().Get() method
-	// http.Reponse struct containing the reponse.
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	// We can then check the value of the response status code and body using
-	// the same pattern as before.
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
-
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
